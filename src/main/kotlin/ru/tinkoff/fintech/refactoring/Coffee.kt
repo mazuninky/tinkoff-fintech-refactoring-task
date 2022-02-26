6e4285aba9d0df075578c08f791d4230e5b2e49d
foxiam <81805420+foxiam@users.noreply.github.com>
2022-02-26 15:31:00 +0300

    hw2 - refactoring

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Coffee.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Coffee.kt
@@ -1,28 +1,13 @@
 package ru.tinkoff.fintech.refactoring
 
-abstract class Coffee(
-    val name: String,
-    val price: Double,
-) {
+data class Coffee(
+    override val name: String,
+    override val price: Double,
+    val brewTimeInMinutes: Int
+) : Product
 
-    companion object {
-        private val coffeeStore = listOf(Espresso(), Cappuccino())
-
-        fun getCoffeeByName(name: String): Coffee? {
-            return coffeeStore.find { it.name == name }
-        }
-    }
-}
-
-class Espresso: Coffee("эспрессо", 5.0)
-class Cappuccino: Coffee("капучино", 3.48)
-
-fun calculateCoffeeBrewTimeInMinutes(
-    coffee: Coffee
-): Int {
-    return when(coffee) {
-        is Espresso -> 5
-        is Cappuccino -> 6
-        else -> error("Неизвестный вид кофе")
+class CoffeeList: ProductList<Coffee>() {
+    fun getCoffeeByName(name: String): Coffee {
+        return getProductByName(name, "Неизвестный вид кофе: $name")
     }
 }
--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Pizza.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Pizza.kt
@@ -1,16 +1,15 @@
 package ru.tinkoff.fintech.refactoring
 
 data class Pizza(
-    val name: String,
-)
+    override val name: String,
+    val ingredients: List<Pair<String, Int>>
+) : Product {
+    override val price: Double = ingredients.sumOf { PizzaStore.ingredients.getIngredientByName(it.first).price * it.second}
+}
 
-fun getIngredient(pizza: Pizza): List<Pair<String, Int>> {
-    return when (pizza.name) {
-        "карбонара" -> listOf("яйца" to 1, "бекон" to 2, "тесто" to 1, "сыр" to 2)
-        "маринара" -> listOf("томат" to 2, "оливки" to 3, "тесто" to 1)
-        "сардиния" -> listOf("салями" to 3, "оливки" to 1, "тесто" to 1, "сыр" to 3)
-        "вальтеллина" -> listOf("вяленая говядина" to 1, "зелень" to 1, "тесто" to 1, "пармезан" to 2)
-        "крестьянская" -> listOf("грибы" to 3, "томат" to 1, "тесто" to 1, "спаржа" to 1, "мясное ассорти" to 1)
-        else -> emptyList()
+class PizzaList: ProductList<Pizza>() {
+    fun getPizzaByName(name: String): Pizza {
+        return getProductByName(name, "Неизвестный вид пиццы: $name")
     }
 }
+
