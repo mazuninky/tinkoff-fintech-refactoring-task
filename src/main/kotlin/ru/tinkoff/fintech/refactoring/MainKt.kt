97d1443f96bd708a1b76d05e59de7a9a089a860f
Kuleshovegor <kuleshovegor2001@gmail.com>
2022-02-27 21:31:20 +0300

    code style changes

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/MainKt.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/MainKt.kt
@@ -4,5 +4,6 @@ fun main() {
     val pizzaStore = PizzaStore()
     val pizzaOrder = pizzaStore.orderPizza("карбонара")
     val coffeeOrder = pizzaStore.orderCoffee("эспрессо")
+
     pizzaStore.executeOrder(pizzaOrder, coffeeOrder)
 }
