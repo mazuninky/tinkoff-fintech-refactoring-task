2df728d23c5cbcfeb41b5cbc4fc431dfb74dafc8
Дима <dima@MacBook-Air-An.local>
2022-03-02 00:38:28 +0300

    project refactored, main.kt was just slightly changed

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Pizza.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Pizza.kt
@@ -1,16 +1,63 @@
 package ru.tinkoff.fintech.refactoring
 
-data class Pizza(
-    val name: String,
-)
+data class Pizza(override val name: String, override val price: Double, val ingredients: Map<Ingredient, Int>) :
+    Product(name, price)
 
-fun getIngredient(pizza: Pizza): List<Pair<String, Int>> {
-    return when (pizza.name) {
-        "карбонара" -> listOf("яйца" to 1, "бекон" to 2, "тесто" to 1, "сыр" to 2)
-        "маринара" -> listOf("томат" to 2, "оливки" to 3, "тесто" to 1)
-        "сардиния" -> listOf("салями" to 3, "оливки" to 1, "тесто" to 1, "сыр" to 3)
-        "вальтеллина" -> listOf("вяленая говядина" to 1, "зелень" to 1, "тесто" to 1, "пармезан" to 2)
-        "крестьянская" -> listOf("грибы" to 3, "томат" to 1, "тесто" to 1, "спаржа" to 1, "мясное ассорти" to 1)
-        else -> emptyList()
+class PizzaDAO {
+    fun getPizzaByName(name: String): Pizza {
+        val ingredientDAO = IngredientDAO()
+        return when (name) {
+            "карбонара" -> {
+                val ingredients = mapOf(
+                    ingredientDAO.getIngredientByName("яйца") to 1,
+                    ingredientDAO.getIngredientByName("бекон") to 2,
+                    ingredientDAO.getIngredientByName("тесто") to 1,
+                    ingredientDAO.getIngredientByName("сыр") to 2
+                )
+                val price = ingredients.map { (ingredient, quantity) -> ingredient.price * quantity }.sum()
+                Pizza(name, price, ingredients)
+            }
+            "маринара" -> {
+                val ingredients = mapOf(
+                    ingredientDAO.getIngredientByName("томат") to 2,
+                    ingredientDAO.getIngredientByName("оливки") to 3,
+                    ingredientDAO.getIngredientByName("тесто") to 1
+                )
+                val price = ingredients.map { (ingredient, quantity) -> ingredient.price * quantity }.sum()
+                Pizza(name, price, ingredients)
+            }
+            "сардиния" -> {
+                val ingredients = mapOf(
+                    ingredientDAO.getIngredientByName("салями") to 3,
+                    ingredientDAO.getIngredientByName("оливки") to 1,
+                    ingredientDAO.getIngredientByName("тесто") to 1,
+                    ingredientDAO.getIngredientByName("сыр") to 3
+                )
+                val price = ingredients.map { (ingredient, quantity) -> ingredient.price * quantity }.sum()
+                Pizza(name, price, ingredients)
+            }
+            "вальтеллина" -> {
+                val ingredients = mapOf(
+                    ingredientDAO.getIngredientByName("вяленая говядина") to 1,
+                    ingredientDAO.getIngredientByName("зелень") to 1,
+                    ingredientDAO.getIngredientByName("тесто") to 1,
+                    ingredientDAO.getIngredientByName("пармезан") to 2
+                )
+                val price = ingredients.map { (ingredient, quantity) -> ingredient.price * quantity }.sum()
+                Pizza(name, price, ingredients)
+            }
+            "крестьянская" -> {
+                val ingredients = mapOf(
+                    ingredientDAO.getIngredientByName("грибы") to 3,
+                    ingredientDAO.getIngredientByName("томат") to 1,
+                    ingredientDAO.getIngredientByName("тесто") to 1,
+                    ingredientDAO.getIngredientByName("спаржа") to 1,
+                    ingredientDAO.getIngredientByName("мясное ассорти") to 1
+                )
+                val price = ingredients.map { (ingredient, quantity) -> ingredient.price * quantity }.sum()
+                Pizza(name, price, ingredients)
+            }
+            else -> error("Неизвестный вид пиццы")
+        }
     }
 }
---
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Product.kt
@@ -0,0 +1,4 @@
+package ru.tinkoff.fintech.refactoring
+
+abstract class Product(open val name: String, open val price: Double)
+
