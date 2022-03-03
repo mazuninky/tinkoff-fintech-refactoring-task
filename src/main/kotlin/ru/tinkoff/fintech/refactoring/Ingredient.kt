9da95355c3837040dea07e0b8f8e5823f395cc3b
micla <adraydante@gmail.com>
2022-03-03 11:44:13 +0300

    Refactoring SOLID

---
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Ingredient.kt
@@ -0,0 +1,17 @@
+package ru.tinkoff.fintech.refactoring
+
+enum class Ingredient(val price: Double) {
+    EGG(3.48),
+    BACON(6.48),
+    DOUGH(1.00),
+    TOMATO(1.53),
+    OLIVES(1.53),
+    CHEESE(0.98),
+    PARMESAN(3.98),
+    MUSHROOMS(3.34),
+    ASPARAGUS(3.34),
+    COULDCUTS(9.38),
+    BEEFJERKY(12.24),
+    SALAMI(13.50),
+    GREENS(3.05)
+}
