20ad25fb1a837f105cded142fd30de0f848cab10
elevante <a.gahverdiev@yandex.ru>
2022-03-03 17:12:17 +0300

    WIP: pizza price counter is improved

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Ingredient.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Ingredient.kt
@@ -2,29 +2,29 @@ package ru.tinkoff.fintech.refactoring
 
 abstract class Ingredient(val name: String, val price: Double, val count: Int = 1)
 
-class Eggs(count: Int = 1) : Ingredient("яйца", 3.48)
+class Eggs(count: Int = 1) : Ingredient("яйца", 3.48 * count)
 
-class Bacon(count: Int = 1) : Ingredient("бекон", 6.48)
+class Bacon(count: Int = 1) : Ingredient("бекон", 6.48 * count)
 
-class Dough(count: Int = 1) : Ingredient("тесто", 1.00)
+class Dough(count: Int = 1) : Ingredient("тесто", 1.00 * count)
 
-class Tomato(count: Int = 1) : Ingredient("томат", 1.53)
+class Tomato(count: Int = 1) : Ingredient("томат", 1.53 * count)
 
-class Olives(count: Int = 1) : Ingredient("оливки", 1.53)
+class Olives(count: Int = 1) : Ingredient("оливки", 1.53 * count)
 
-class Cheese(count: Int = 1) : Ingredient("сыр", 0.98)
+class Cheese(count: Int = 1) : Ingredient("сыр", 0.98 * count)
 
-class Parmesan(count: Int = 1) : Ingredient("пармезан", 3.98)
+class Parmesan(count: Int = 1) : Ingredient("пармезан", 3.98 * count)
 
-class Mushrooms(count: Int = 1) : Ingredient("грибы", 3.34)
+class Mushrooms(count: Int = 1) : Ingredient("грибы", 3.34 * count)
 
-class Asparagus(count: Int = 1) : Ingredient("спаржа", 3.34)
+class Asparagus(count: Int = 1) : Ingredient("спаржа", 3.34 * count)
 
-class Meet(count: Int = 1) : Ingredient("мясное ассорти", 9.38)
+class Meet(count: Int = 1) : Ingredient("мясное ассорти", 9.38 * count)
 
-class BeefJerky(count: Int = 1) : Ingredient("вяленая говядина", 12.24)
+class BeefJerky(count: Int = 1) : Ingredient("вяленая говядина", 12.24 * count)
 
 //Unspecified price
-class Greenery(count: Int = 1) : Ingredient("зелень", 0.001)
+class Greenery(count: Int = 1) : Ingredient("зелень", 0.001 * count)
 
-class Salami(count: Int = 1) : Ingredient("салями", 0.001)
+class Salami(count: Int = 1) : Ingredient("салями", 0.001 * count)
