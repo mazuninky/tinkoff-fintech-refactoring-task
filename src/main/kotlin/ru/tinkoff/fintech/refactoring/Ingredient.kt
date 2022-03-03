7adbaa59df24fc04ab04bac10f61dd2b5a8ff813
elevante <a.gahverdiev@yandex.ru>
2022-03-03 16:15:34 +0300

    WIP: adding ingredients and pizza

---
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Ingredient.kt
@@ -0,0 +1,31 @@
+package ru.tinkoff.fintech.refactoring
+
+abstract class Ingredient(val name: String, val price: Double, val count: Int = 1)
+
+class Eggs(count: Int = 1) : Ingredient("яйца", 3.48)
+
+class Bacon(count: Int = 1) : Ingredient("бекон", 6.48)
+
+class Dough(count: Int = 1) : Ingredient("тесто", 1.00)
+
+class Tomato(count: Int = 1) : Ingredient("томат", 1.53)
+
+class Olives(count: Int = 1) : Ingredient("оливки", 1.53)
+
+class Cheese(count: Int = 1) : Ingredient("сыр", 0.98)
+
+class Parmesan(count: Int = 1) : Ingredient("пармезан", 3.98)
+
+class Mushrooms(count: Int = 1) : Ingredient("грибы", 3.34)
+
+class Asparagus(count: Int = 1) : Ingredient("спаржа", 3.34)
+
+class Meet(count: Int = 1) : Ingredient("мясное ассорти", 9.38)
+
+class BeefJerky(count: Int = 1) : Ingredient("вяленая говядина", 12.24)
+
+
+//Unspecified price
+class Greenery(count: Int = 1) : Ingredient("зелень", 0.001)
+
+class Salami(count: Int = 1) : Ingredient("салями", 0.001)
--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Pizza.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Pizza.kt
@@ -1,16 +1,19 @@
 package ru.tinkoff.fintech.refactoring
 
-data class Pizza(
+abstract class Pizza(
     val name: String,
-)
-
-fun getIngredient(pizza: Pizza): List<Pair<String, Int>> {
-    return when (pizza.name) {
-        "карбонара" -> listOf("яйца" to 1, "бекон" to 2, "тесто" to 1, "сыр" to 2)
-        "маринара" -> listOf("томат" to 2, "оливки" to 3, "тесто" to 1)
-        "сардиния" -> listOf("салями" to 3, "оливки" to 1, "тесто" to 1, "сыр" to 3)
-        "вальтеллина" -> listOf("вяленая говядина" to 1, "зелень" to 1, "тесто" to 1, "пармезан" to 2)
-        "крестьянская" -> listOf("грибы" to 3, "томат" to 1, "тесто" to 1, "спаржа" to 1, "мясное ассорти" to 1)
-        else -> emptyList()
-    }
+    val ingredients: List<Ingredient>
+) {
+    val price: Double = ingredients.sumOf {it.price}
 }
+
+
+class Carbonara : Pizza("карбонара", listOf(Eggs(1), Bacon(2), Dough(1), Cheese(2)))
+
+class Marinara : Pizza("маринара", listOf(Tomato(1), Olives(3), Dough(1)))
+
+class Sardinia : Pizza("сардиния", listOf(Salami(3), Olives(1), Dough(1), Cheese(3)))
+
+class Valtellina : Pizza("вальтеллина", listOf(BeefJerky(1), Greenery(1), Dough(1), Parmesan(2)))
+
+class Peasant : Pizza("крестьянская", listOf(Mushrooms(3), Tomato(1), Dough(1), Asparagus(1), Meet(1)))
