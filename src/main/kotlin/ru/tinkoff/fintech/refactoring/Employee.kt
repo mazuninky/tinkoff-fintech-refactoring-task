a7c387a8f051c3a85d219058cbda312f48fc5579
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-03-04 21:43:01 +0300

    поправил на (ingredient, amount)

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Employee.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Employee.kt
@@ -43,12 +43,12 @@ class PizzaMaker : Employee {
         var pizzaPrice = 0.0
         var ingredientCounterAndTime = 0
         println("$title Делаю пиццу: ${pizza.pizzaName}")
-        println("$title Из ингридиетов:")
-        pizza.ingredients.forEach {
-            val price = ingredientsPrice[it.key] ?: error("Неизвестный ингредиент")
-            println("$title - ${it.key}: в количестве ${it.value} за $price$")
-            pizzaPrice += price * it.value
-            ingredientCounterAndTime += it.value
+        println("$title Из ингредиентов:")
+        pizza.ingredients.forEach { (ingredient, amount) ->
+            val price = ingredientsPrice[ingredient] ?: error("Неизвестный ингредиент")
+            println("$title - $ingredient: в количестве $amount за $price$")
+            pizzaPrice += price * amount
+            ingredientCounterAndTime += amount
         }
         val roundedPrice = "%.2f".format(pizzaPrice)
         println("$title время приготовления $ingredientCounterAndTime минут")
