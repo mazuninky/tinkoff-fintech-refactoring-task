bd1333a07cc0d26dc6543c10b5a7c9250cda5b80
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2022-03-04 20:16:37 +0300

    Add Coffee-class

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/Barista.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/Barista.kt
@@ -3,16 +3,23 @@ package ru.tinkoff.fintech.refactoring
 class Barista {
     fun makeCoffee(order: Order) {
         val coffee = coffeeMenu[order.name] ?: error("Невозможно приготовить \"${order.name}\"")
-        val price = coffee.second
+        val price = coffee.price
 
         println("[Бариста] Готовлю напиток: ${order.name}")
-        println("[Бариста] Время приготовления: ${coffee.first} минут")
+        println("[Бариста] Время приготовления: ${coffee.brewTime} минут")
         val roundedPrice = "%.2f".format(price)
         println("[Бариста] Стоимость напитка: $roundedPrice$")
     }
 }
 
-private val coffeeMenu = mapOf(
-    "эспрессо" to Pair(5, 5.0),
-    "капучино" to Pair(6, 3.48),
+
+private val coffeeMenu = listOf(
+    Coffee("эспрессо", 5, 5.0),
+    Coffee("капучино", 6, 3.48),
+).associateBy { it.name }
+
+private data class Coffee(
+    val name: String,
+    val brewTime: Int,
+    val price: Double,
 )
