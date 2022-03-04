99d82e78a2c6c5abd151a5b60f07fc87f9323207
GrushinDmitry <dmitriyrzn1991@gmail.com>
2022-03-04 22:47:34 +0300

    создал поля в классе пицерии и убрал передачу в функцию функцию

--- src/main/kotlin/ru/tinkoff/fintech/refactoring/PizzaStore.kt
+++ src/main/kotlin/ru/tinkoff/fintech/refactoring/PizzaStore.kt
@@ -1,10 +1,16 @@
 package ru.tinkoff.fintech.refactoring
 
 class PizzaStore {
+    private val pizzaMaker = PizzaMaker()
+
+    private val barista = Barista()
+
+    private val cleaner = Cleaner()
 
     var orderNumber = 0
 
-    private val ingredientsPrice = mapOf("яйца" to 3.48,
+    private val ingredientsPrice = mapOf(
+        "яйца" to 3.48,
         "бекон" to 6.48,
         "тесто" to 1.00,
         "томат" to 1.53,
@@ -16,12 +22,17 @@ class PizzaStore {
         "мясное ассорти" to 9.38,
         "вяленая говядина" to 12.24)
 
-    fun executeOrderPizza(pizzaName: String) =
-        PizzaMaker().makePizza(++orderNumber, Pizza.getPizzaByName(pizzaName), ingredientsPrice)
+    fun executeOrderPizza(pizzaName: String) {
+        val pizza = Pizza.getPizzaByName(pizzaName)
 
+        return pizzaMaker.makePizza(++orderNumber, pizza, ingredientsPrice)
+    }
 
-    fun executeOrderCoffee(coffeeName: String) =
-        Barista().makeCoffee(++orderNumber, Coffee.getCoffeeByName(coffeeName))
+    fun executeOrderCoffee(coffeeName: String) {
+        val coffee = Coffee.getCoffeeByName(coffeeName)
 
-    fun executeCleaning() = Cleaner().cleanFloor()
+        return barista.makeCoffee(++orderNumber, coffee)
+    }
+
+    fun executeCleaning() = cleaner.cleanFloor()
 }
